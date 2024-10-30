23f2224e565f46b0756733b936929f3740c74c2f
fodiwuor <orwafredrick97@gmail.com>
2024-10-30 17:48:43 +0300

    Update FredTrainningscript.R

--- codes/FredTrainningscript.R
+++ codes/FredTrainningscript.R
@@ -1,13 +1,1 @@
-setwd("C:/Users/THINKPAD/Desktop/PleliminaryAnalysis")
-admin <- read_dta("Data/temp/admin.dta")##import data
-##generate resident status
-admin$resident<-NULL
-admin$resident<-NA
-admin$resident[!is.na(admin$fk_res)]<-1
-admin$resident[is.na(admin$fk_res)]<-0
-admin$resident<-factor(admin$resident,levels=c(1,0),labels =c("dss","non-dss") )
 
-##view resident
-View(admin%>%select(resident,fk_res,everything()))
-##Table
-table(admin$resident)
